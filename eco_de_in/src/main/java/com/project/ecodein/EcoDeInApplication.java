92eaeafc8838a1ab5b429f08d2e3c2eecb41f6f0
DESKTOP-M6NF7J2\hi <didgkswo123@gmail.com>
2024-09-13 11:06:04 +0900

    첫 배포

---
+++ eco_de_in/src/main/java/com/project/ecodein/EcoDeInApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecodein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoDeInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoDeInApplication.class, args);
+	}
+
+}
---
+++ eco_de_in/src/test/java/com/project/ecodein/EcoDeInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecodein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDeInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
