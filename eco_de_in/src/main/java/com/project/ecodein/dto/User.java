26dc2d0d590f328b637ce83b9b8a7b1e85d5f5f4
Jangtaeil <bigonej3@naver.com>
2024-09-13 18:10:18 +0900

    #005 [장태일] dto 작업{재고관리, 창고관리, 회원}

---
+++ eco_de_in/src/main/java/com/project/ecodein/dto/User.java
@@ -0,0 +1,46 @@
+package com.project.ecodein.dto;
+
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
+public class User {
+	
+	@Id
+	@Column(name="user_id", nullable=false)
+	private String user_id;
+	
+	@ManyToOne
+	@JoinColumn(referencedColumnName="buyer_code", nullable = false)
+	private int buyer_code;
+
+	@Column(name="user_password", nullable = false)
+	private String user_password;
+	
+	@Column(name="user_name", nullable = false)
+	private String user_name;
+
+	@Column(name="user_date", nullable = false)
+	private Date user_date;
+
+	@Column(name = "user_email", nullable = false)
+	private String user_email;
+
+}
+
