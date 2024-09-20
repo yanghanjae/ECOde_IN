a3c772f78971f0afc489d81e72e06afbeec3d860
yubin0908 <dasol_b@kakao.com>
2024-09-20 15:35:54 +0900

    #012 [장유빈] Sidebar 수정 및 컨텐츠 영역 조절, 거래처 관리 등록기능 및 수정 모달, 상태 변경 기능 추가

---
+++ eco_de_in/src/main/java/com/project/ecodein/dto/Search.java
@@ -0,0 +1,14 @@
+package com.project.ecodein.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Search {
+
+	private String keyword;
+
+}
--- eco_de_in/src/main/java/com/project/ecodein/repository/BuyerRepository.java
+++ eco_de_in/src/main/java/com/project/ecodein/repository/BuyerRepository.java
@@ -1,24 +1,31 @@
 package com.project.ecodein.repository;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-=======
->>>>>>> 1677ce76b4ad9895d4cf2aabe632bb40c7b61384
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.project.ecodein.dto.Buyer;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {
-<<<<<<< HEAD
 
-	@Query (value = "select * from buyer where buyer_name = ? or buyer_number = ?", nativeQuery = true)
-	public List<Buyer> findByBuyerNameOrBuyerNumber (String keyword);
+	// 검색 시 실행되는 코드
+	@Query (value = "select * from buyer where buyer_status = :status and buyer_name like concat('%', :keyword, '%') or buyer_number like concat('%', :keyword, '%')", nativeQuery = true)
+	public Page<Buyer> findByBuyerStatusAndBuyerNameOrBuyerNumber (@Param ("keyword") String keyword,
+		@Param ("status") int status,
+		Pageable pageable);
 
-=======
+	// 기본 실행 코드(조회)
+	@Query (value = "select * from buyer where buyer_status = :status", nativeQuery = true)
+	public Page<Buyer> findByBuyerStatus (Pageable pageable, @Param ("status") int status);
 	
-	List<Buyer> findAll();
-		
->>>>>>> 1677ce76b4ad9895d4cf2aabe632bb40c7b61384
+	// 상태 수정
+	@Query (value = "update buyer set buyer_status = 0 where buyer_code = :buyer_code", nativeQuery = true)
+	@Modifying @Transactional
+	public void updateBuyerStatusByBuyerCode (@Param ("buyer_code") Long buyer_code);
+
 }
