bd6607ddfa0396daf7c95945c5173fe657e164c7
yubin0908 <dasol_b@kakao.com>
2024-09-27 13:09:58 +0900

    #040 [장유빈] 사이드바 bug fix - 컨텐츠 간섭 제거

--- eco_de_in/src/main/resources/static/js/header-visible-toggle.js
+++ eco_de_in/src/main/resources/static/js/header-visible-toggle.js
@@ -1,8 +1,9 @@
 function sidebar_not_visible() {
   document.querySelector('#header').classList.toggle('not-visible');
   document.querySelector('.header').classList.toggle('active-header');
+  document.querySelector('.middle').classList.toggle('center-menu');
   document.querySelectorAll('.text').forEach(t => t.classList.toggle('deActive'));
-  document.querySelectorAll('.menu').forEach(m => m.classList.toggle('text-center'));
+  document.querySelectorAll('.menu').forEach(m => m.classList.toggle('center-menu'));
   document.querySelector('.user-info').classList.toggle('deActive');
   document.querySelector('img[alt="logout-btn"]').classList.toggle('deActive');
   document.querySelector('.footer').classList.toggle('not-visible');
