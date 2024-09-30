614026e56fef3656da9ccc14ea1f7aac67517f9a
yubin0908 <dasol_b@kakao.com>
2024-09-30 12:13:20 +0900

    #048 [장종락] 회원 비밀번호 기능 추가

--- eco_de_in/src/main/java/com/project/ecodein/controller/UserController.java
+++ eco_de_in/src/main/java/com/project/ecodein/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.project.ecodein.controller;
 
 import com.project.ecodein.dto.Admin;
+import com.project.ecodein.dto.User;
 import com.project.ecodein.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,22 +15,22 @@ import java.util.Optional;
 @Slf4j
 public class UserController {
 
-    private final UserService userService;
+    private final UserService USER_SERVICE;
 
     public UserController(UserService userService) {
-        this.userService = userService;
+        this.USER_SERVICE = userService;
     }
 
     @GetMapping("/admin-manager")
     @ResponseBody
     public List<Admin> adminList() {
-        return userService.findAllByAdminRecognize();
+        return USER_SERVICE.findAllByAdminRecognize();
     }
 
     @GetMapping("/admin-manager/{admin_id}")
     public String adminAuth(@PathVariable String admin_id, Model model) {
 
-        userService.adminAuthPass (admin_id);
+        USER_SERVICE.adminAuthPass (admin_id);
         model.addAttribute("auth_message", "관리자 승인이 완료되었습니다.");
 
         return "redirect:/main";
@@ -44,4 +42,18 @@ public class UserController {
         modal.addAttribute("auth", "user-management");
         return "functional/modal";
     }
+
+    @PostMapping("/user_management")
+    @ResponseBody
+    public Optional<User> userManagement(String user_id, String user_password) {
+        return USER_SERVICE.userPasswordCheck(user_id, user_password);
+    }
+
+    @PostMapping("/user_password_modify")
+    @ResponseBody
+    public Integer userPasswordModify(String user_id, String user_password) {
+        return USER_SERVICE.userPasswordModify(user_id, user_password);
+    }
+
+
 }
--- eco_de_in/src/main/java/com/project/ecodein/service/UserService.java
+++ eco_de_in/src/main/java/com/project/ecodein/service/UserService.java
@@ -1,9 +1,10 @@
 package com.project.ecodein.service;
 
 import com.project.ecodein.dto.Admin;
+import com.project.ecodein.dto.User;
 import com.project.ecodein.repository.AdminRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.Modifying;
+import com.project.ecodein.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +13,41 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    private final AdminRepository adminRepository;
+    private final AdminRepository ADMIN_REPOSITORY;
+    private final UserRepository USER_REPOSITORY;
+    private final PasswordEncoder PASSWORD_ENCODER;
 
-    public UserService(AdminRepository adminRepository) {
-        this.adminRepository = adminRepository;
+    public UserService(AdminRepository adminRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.ADMIN_REPOSITORY = adminRepository;
+        this.USER_REPOSITORY = userRepository;
+        this.PASSWORD_ENCODER = passwordEncoder;
     }
 
     // [240927] 장유빈 기능 추가
     // 기능 상세 : 관리자 메뉴 -> 관리자 승인 요청 조회 기능
     public List<Admin> findAllByAdminRecognize () {
-        return adminRepository.findByAdminRecognize();
+        return ADMIN_REPOSITORY.findByAdminRecognize();
     }
 
     public void adminAuthPass (String adminId) {
-        adminRepository.updateAdminAuthByAdminId(adminId);
+        ADMIN_REPOSITORY.updateAdminAuthByAdminId(adminId);
+    }
+
+    public Optional<User> userPasswordCheck (String user_id, String user_password) {
+        User user = USER_REPOSITORY.findById(user_id).get();
+
+        String _password = user.getUser_password();
+
+        if (PASSWORD_ENCODER.matches(user_password, _password)) {
+            return Optional.of(user);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Integer userPasswordModify (String user_id, String user_password) {
+        String newPassword = PASSWORD_ENCODER.encode(user_password);
+
+        return USER_REPOSITORY.updatePassword(user_id, newPassword);
     }
 }
