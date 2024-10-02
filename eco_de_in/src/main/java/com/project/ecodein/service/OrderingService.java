bf67f5e79e353237aff3c6486bc1a78584994741
송서현 <shhsw123@gmail.com>
2024-10-02 09:38:04 +0900

    #61 [송서현] 발주등록 기능구현중

--- eco_de_in/src/main/java/com/project/ecodein/service/OrderingService.java
+++ eco_de_in/src/main/java/com/project/ecodein/service/OrderingService.java
@@ -1,6 +1,12 @@
 package com.project.ecodein.service;
 
+import com.project.ecodein.dto.Item;
+import com.project.ecodein.dto.Stock;
+import com.project.ecodein.dto.Storage;
+import com.project.ecodein.repository.ItemRepository;
 import com.project.ecodein.repository.OrderingRepository;
+import com.project.ecodein.repository.StockRepository;
+import com.project.ecodein.repository.StorageRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -10,16 +16,25 @@ import org.springframework.stereotype.Service;
 
 import com.project.ecodein.dto.Ordering;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class OrderingService {
 
 
 	private final OrderingRepository ORDERING_REPOSITORY;
+    private final StockRepository STOCK_REPOSITORY;
+    private final StorageRepository STORAGE_REPOSITORY;
+    private final ItemRepository ITEM_REPOSITORY;
 
-	public OrderingService(OrderingRepository orderingRepository) {
-
-		this.ORDERING_REPOSITORY = orderingRepository;
-	}
+    public OrderingService(OrderingRepository orderingRepository, StockRepository stockRepository,
+                           StorageRepository storageRepository, ItemRepository itemRepository) {
+        this.ORDERING_REPOSITORY = orderingRepository;
+        this.STOCK_REPOSITORY = stockRepository;
+        this.STORAGE_REPOSITORY = storageRepository;
+        this.ITEM_REPOSITORY = itemRepository;
+    }
 
 	// 페이지네이션 및 검색 기능 구현
 	public Page<Ordering> getOrders(@Param("page") int page, @Param("query") String query,
@@ -53,43 +68,48 @@ public class OrderingService {
         }
 	}
 
-
-	// (필터검색)_페이지네이션 및 검색 기능 구현
-//	public Page<Ordering> getOrders(int page, String query, String status) {
-//		Sort sort = Sort.by(Sort.Order.desc("orderNo")); // 최신 발주 번호 순으로 정렬
-//		Pageable pageable = PageRequest.of(page - 1, 10, sort); // 페이지당 10개씩 조회
-//
-//		// 상태에 따른 조건 처리
-//		if (status != null && !status.isEmpty()) {
-//			switch (status) {
-//				case "in-progress":
-//					return ORDERING_REPOSITORY.findByIsDeliveryFalse(pageable); // 진행중인 발주
-//				case "completed":
-//					return ORDERING_REPOSITORY.findByIsDeliveryTrue(pageable); // 완료된 발주
-//				case "all":
-//				default:
-//					return ORDERING_REPOSITORY.findAll(pageable); // 전체 발주
-//			}
-//		}
-//
-//		// 검색어가 없으면 전체 조회, 검색어가 있으면 조건 검색
-//		if (query == null || query.isEmpty()) {
-//			return ORDERING_REPOSITORY.findAll(pageable); // 검색어가 없을 때 전체 조회
-//		} else {
-//			return ORDERING_REPOSITORY.searchByQuery(query, pageable); // 검색어가 있을 때 조건 검색
-//		}
-//	}
-
-
-
-	// 검색 조건에 따른 발주 목록 조회 (삭제 ? )
-//	public Page<Ordering> searchOrders(String query) {
-//		return ORDER_REPOSITORY.searchByQuery(query, pageable);
-//	}
-
 	// 삭제 기능
 	public void deleteOrder(int orderNo) {
 		ORDERING_REPOSITORY.deleteById(orderNo);
 	}
+
+    // 재고 추가 및 업데이트
+    public void addStock(int item_no, int storage_no, int quantity) {
+        Optional<Stock> stock = STOCK_REPOSITORY.findStock(item_no, storage_no);
+        if (stock.isPresent()) {
+            STOCK_REPOSITORY.updateStock(stock.get().getStockNo(), stock.get().getQuantity() + quantity);
+        } else {
+            STOCK_REPOSITORY.addStock(item_no, storage_no, quantity);
+        }
+    }
+
+    // 재고 조회
+    public List<Storage> findAllStorage() {
+        return STORAGE_REPOSITORY.findAllStorage();
+    }
+
+    public Optional<Stock> findByStockNo(int stock_no) {
+        return STOCK_REPOSITORY.findByStockNO(stock_no);
+    }
+
+    public void editStock(int editStockNo, int editQuantity) {
+        STOCK_REPOSITORY.updateStock(editStockNo, editQuantity);
+    }
+
+    public Page<Item> searchStocks(String search, int page) {
+        Pageable pageable = PageRequest.of(page - 1, 10, Sort.by("item_name").ascending());
+        if (search == null || search.isEmpty()) {
+            search = "";
+        }
+        return ITEM_REPOSITORY.searchItem(search, pageable);
+    }
+
+    public Page<Storage> searchStorages(String search, int page) {
+        Pageable pageable = PageRequest.of(page - 1, 10, Sort.by("storage_name").ascending());
+        if (search == null || search.isEmpty()) {
+            search = "";
+        }
+        return STORAGE_REPOSITORY.findAllByStorageNameOrStorageSite(search, pageable);
+    }
 
 }
